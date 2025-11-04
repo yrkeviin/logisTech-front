7150864c5fdc653a2957e2344049dda4f99c2eb3
yrkeviin <kevineziquiellima160@gmail.com>
2025-11-04 15:15:28 -0300

    Feat: Page informacoes and infos

---
+++ src/app/informacoes/page.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './page.module.css'
+import HeaderLogis from "../../components/HeaderLogis/HeaderLogis"
+import CardMini from '../../components/CardMini/CardMini'
+
+export default function Informacoes() {
+  return (
+    <div className={styles.container}>
+        <HeaderLogis />
+
+        <h1 className={styles.title}>Informações das Entregas:</h1>
+
+      <div className={styles.cards}>
+        <CardMini
+        icon="/image/icon1.png"
+        number="2345"
+        description="Pedidos Totais"
+        />
+        <CardMini
+        icon="/image/icon2.png"
+        number="2345"
+        description="Pedidos Totais"
+        />
+        <CardMini
+        icon="/image/icon3.png"
+        number="2345"
+        description="Pedidos Totais"
+        />
+        <CardMini
+        icon="/image/icon4.png"
+        number="2345"
+        description="Pedidos Totais"
+        />
+      </div>
+    </div>
+  )
+}
