13edb9789b8f346e8fee539f7d16923fe6bc90ef
yrkeviin <kevineziquiellima160@gmail.com>
2025-11-04 11:29:08 -0300

    Feat: Dashboard section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,23 @@ export default function HomeAdm() {
   return (
     <div className={styles.container}>
       <Header />
+
+      <div className={styles.dashboard}>
+        <div className={styles.quadrados}>
+          <div className={styles.quadrados1}>
+            <div className={styles.quadrado}></div>
+            <div className={styles.quadrado}></div>
+          </div>
+
+          <div className={styles.quadrados2}>
+            <div className={styles.quadrado}></div>
+            <div className={styles.quadrado}></div>
+          </div>
+        </div>
+
+        <h1>Dashboard:</h1>
+      </div>
+
     </div>
   )
 }
